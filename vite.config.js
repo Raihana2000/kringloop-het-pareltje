28d961dad6e7bfee5fd152f32c0343b9a80a31d0
Raihana2000 <122817234+Raihana2000@users.noreply.github.com>
2026-06-03 20:33:05 +0200

    Update Vite build settings

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [react()],
   build: {
-    outDir: 'docs'
+    outDir: 'docs',
+    emptyOutDir: true
   },
   server: {
     host: '127.0.0.1',
