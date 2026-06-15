f595bb2176092af4e6a0dd26a0ca9b72999e475b
Raihana2000 <122817234+Raihana2000@users.noreply.github.com>
2026-06-15 22:44:41 +0200

    Update Facebook page link

--- src/App.jsx
+++ src/App.jsx
@@ -82,7 +82,7 @@ const mapsEmbed =
 const socialLinks = [
   {
     label: 'Facebook',
-    href: 'https://www.facebook.com/kringloophalfzwaan/?notif_id=1781097853988362&notif_t=page_user_activity&ref=notif
+    href: 'https://www.facebook.com/kringloophalfzwaan/
     aria: 'Facebook van Kringloop Het Pareltje',
     className: 'social-button--facebook',
     icon: (
